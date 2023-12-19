cedab8a51630b42b6254e8631dc888b757d06bf5
project <139109816+seyyedparsalotfy@users.noreply.github.com>
2023-12-19 10:13:31 +0000

    Update App.js and index.js

--- parsa/src/App.js
+++ parsa/src/App.js
@@ -1,8 +1,22 @@
+import React from "react";
 import { TypeAnimation } from "react-type-animation";
-export default SSY = () => {
+export const SSY = () => {
   return (
     <TypeAnimation
       sequence={["ما پیشگامان آینده دیجیتال هستیم!", 1000]}
+      wrapper="span"
+      speed={50}
+      style={{ fontSize: "2em", display: "inline-block" }}
+    />
+  );
+};
+export const MMY = () => {
+  return (
+    <TypeAnimation
+      sequence={[
+        "طراحی سایت ، طراحی رابط و تجربه کاربری ، دیجیتال مارکتینگ",
+        1000,
+      ]}
       wrapper="span"
       speed={50}
       style={{ fontSize: "2em", display: "inline-block" }}
--- parsa/src/index.js
+++ parsa/src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import SSY from "./App";
+import { SSY, MMY } from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(<SSY />, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <SSY />
+    <MMY />
+  </div>,
+  document.getElementById("root"),
+);
 reportWebVitals();
