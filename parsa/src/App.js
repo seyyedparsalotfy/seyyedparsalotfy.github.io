42b9424e76b35abeebb87b85959889bc9de28354
project <139109816+seyyedparsalotfy@users.noreply.github.com>
2023-12-19 09:43:34 +0000

    Update App.js and index.js

--- parsa/src/App.js
+++ parsa/src/App.js
@@ -1,2 +1,11 @@
-const app = () => {};
-export default app;
+import { TypeAnimation } from "react-type-animation";
+export default SSY = () => {
+  return (
+    <TypeAnimation
+      sequence={["ما پیشگامان آینده دیجیتال هستیم!", 1000]}
+      wrapper="span"
+      speed={50}
+      style={{ fontSize: "2em", display: "inline-block" }}
+    />
+  );
+};
