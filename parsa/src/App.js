ceb737c45cf7306432b492afbb4b62e89405fa2f
project <139109816+seyyedparsalotfy@users.noreply.github.com>
2023-12-20 15:15:44 +0000

    Update 4 files

--- parsa/src/App.js
+++ parsa/src/App.js
@@ -5,15 +5,23 @@ import "./App.css";
 const MMY = () => {
   return (
     <TypeAnimation
-      style={{ whiteSpace: "pre-line", height: "195px", display: "block" }}
+      style={{
+        whiteSpace: "pre-line",
+        height: "195px",
+
+        color: "white",
+        fontSize: "25px",
+        margin: "0",
+        padding: "0",
+      }}
       sequence={[
-        `ما پیشگامان آینده دیجیتال هستیم!\n
+        `ما پیشگامان آینده دیجیتال هستیم!             \n
           
-        طراحی سایت ، طراحی رابط و تجربه کاربری ، دیجیتال مارکتینگ`,
+        طراحی سایت ، طراحی رابط و تجربه کاربری ، دیجیتال مارکتینگ\n 
+        
+        founded by seyyed parsa lotfy`,
         1000,
-        "",
       ]}
-      repeat={Infinity}
     />
   );
 };
--- parsa/src/index.js
+++ parsa/src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { MMY } from "./App";
+import  MMY from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./App.css";
-ReactDOM.render(<MMY /> document.getElementById("root"),
-);
+ReactDOM.render(<MMY/>, document.getElementById("root"));
 reportWebVitals();
