005b4478a6c4a4b7210e9b439ff94a8a967753fe
project <139109816+seyyedparsalotfy@users.noreply.github.com>
2023-12-20 12:18:41 +0000

    Update 5 files

--- parsa/src/App.js
+++ parsa/src/App.js
@@ -1,46 +1,20 @@
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
 import "./App.css";
-export const SSY = () => {
+
+const MMY = () => {
   return (
     <TypeAnimation
-      sequence={["ما پیشگامان آینده دیجیتال هستیم!", 1000]}
-      speed={350}
-      style={{
-        fontSize: "2em",
-        whiteSpace: "pre-line",
-        height: "195px",
-        display: "inline-block",
-        position: "absolute",
-
-        top: "65%",
-
-        borderradius: "1em",
-        color: "white",
-      }}
-    />
-  );
-};
-export const MMY = () => {
-  return (
-    <TypeAnimation
+      style={{ whiteSpace: "pre-line", height: "195px", display: "block" }}
       sequence={[
-        "طراحی سایت ، طراحی رابط و تجربه کاربری ، دیجیتال مارکتینگ",
+        `ما پیشگامان آینده دیجیتال هستیم!\n
+          
+        طراحی سایت ، طراحی رابط و تجربه کاربری ، دیجیتال مارکتینگ`,
         1000,
+        "",
       ]}
-      speed={350}
-      style={{
-        fontSize: "2em",
-        whiteSpace: "pre-line",
-        height: "195px",
-        display: "inline-block",
-        position: "relative",
-
-        top: "65%",
-
-        borderradius: "1em",
-        color: "white",
-      }}
+      repeat={Infinity}
     />
   );
 };
+export default MMY;
--- parsa/src/index.js
+++ parsa/src/index.js
@@ -1,14 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { SSY, MMY } from "./App";
+import { MMY } from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./App.css";
-ReactDOM.render(
-  <div>
-    <SSY />
-    <MMY />
-  </div>,
-  document.getElementById("root"),
+ReactDOM.render(<MMY /> document.getElementById("root"),
 );
 reportWebVitals();
